6940fbf1add388bac442716f45671b8233a61f73
Alex Chism <37649529+achism@users.noreply.github.com>
2026-03-27 21:50:32 -0700

    Rename main to hello; add temperature converter skeleton

---
+++ temp.c
@@ -0,0 +1,11 @@
+/* Fahrenheit to Celsius Temperature Converter */
+
+#include <stdio.h>
+
+int main(void)
+{
+	/* Prompt the user to enter a temperature value in Fahrenheit */
+	/* Convert from Fahrenheit to Celsius */
+	/* Print the corresponding temperature value in Celsius */
+	return 0;
+}
